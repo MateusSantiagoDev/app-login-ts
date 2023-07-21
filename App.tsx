91016005024d5efa3f642ec56c9d63315d798cd3
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-07-21 14:47:26 -0300

    feat: fonts configuradet

--- App.tsx
+++ App.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import { ActivityIndicator, View } from "react-native";
+
 import { ThemeProvider } from "styled-components/native";
 import theme from "./src/theme";
-import { Text, View } from "react-native";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { SignIn } from "@screens/SignIn";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar
-      style="light"
-      backgroundColor="transparent"
-      translucent
-      />
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+      {fontsLoaded ? (
+        <SignIn />
+      ) : (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <ActivityIndicator color="#166534" size="large" />
+        </View>
+      )}
     </ThemeProvider>
   );
 }
