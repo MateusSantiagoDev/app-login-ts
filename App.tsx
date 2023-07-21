daeec92fef6b4469237011751d109af7a8ff6e6f
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-07-21 13:53:23 -0300

    chore: initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar style="auto" />
+      <Text>login app!!</Text>
+    </View>
+  );
+}
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ts", ".tsx", ".js", ".json"],
+          alias: {
+            "@hooks": "./src/hooks",
+            "@screens": "./src/screens",
+            "@theme": "./src/theme",
+          },
+        },
+      ],
+    ],
   };
 };
